dd79a20f22a8d5077109abd0654598a41df18600
Mahmoud Mohamed Ramadan <mahmoudramadan496@gmail.com>
2020-09-22 23:16:07 +0200

    add delegate trait

---
+++ app/Traits/Delegate/ValidateDelegateTrait.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Traits\Delegate;
+
+trait ValidateDelegateTrait {
+  public function validation()
+  : array {
+    return request()->validate([
+      'name'   => 'required', 'national_id' => 'required|numeric',
+      'phone'  => 'required|numeric', 'motor_size' => 'required|numeric',
+      'car_id' => 'required', 'made_date' => 'required',
+      'image'  => 'sometimes|file|image|max: 2000|unique:delegates,image'
+    ]);
+  }
+}
