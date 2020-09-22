8df47168476970ebcec6ed1a6c9c890de4442674
Mahmoud Mohamed Ramadan <mahmoudramadan496@gmail.com>
2020-09-22 23:09:29 +0200

    add user notification

---
+++ app/Notifications/UserNotification.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class UserNotification extends Notification {
+  use Queueable;
+
+  private $data;
+
+  /**
+   * Create a new notification instance.
+   *
+   * @return void
+   */
+  public function __construct($data) {
+    $this->data = $data;
+  }
+
+  /**
+   * Get the notification's delivery channels.
+   *
+   * @param mixed $notifiable
+   * @return array
+   */
+  public function via($notifiable) {
+    return ['database'];
+  }
+
+  /**
+   * Get the mail representation of the notification.
+   *
+   * @param mixed $notifiable
+   * @return \Illuminate\Notifications\Messages\MailMessage
+   */
+  public function toMail($notifiable) {
+    return (new MailMessage)->markdown('notify.notification');
+  }
+
+  /**
+   * Get the array representation of the notification.
+   *
+   * @param mixed $notifiable
+   * @return array
+   */
+  public function toArray($notifiable) {
+    return [
+
+    ];
+  }
+
+  /**
+   *
+   */
+  public function toDatabase($notifiable) {
+    return [
+      'name'    => $this->data['name'], 'email' => $this->data['email'],
+      'message' => $this->data['message']
+    ];
+  }
+}
