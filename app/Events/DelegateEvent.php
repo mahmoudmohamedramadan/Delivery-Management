538b04b738a5611f4deb8dcec475d8d7dd6554ba
Mahmoud Mohamed Ramadan <mahmoudramadan496@gmail.com>
2020-09-22 22:42:00 +0200

    add DelegateEvent to add new delegate

---
+++ app/Events/DelegateEvent.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class DelegateEvent implements ShouldQueue {
+  use Dispatchable, InteractsWithSockets, SerializesModels;
+
+  //when set property to public it will be visible at listener
+  public $data;
+
+  /**
+   * Create a new event instance.
+   *
+   * @param $data
+   */
+  public function __construct($data) {
+    $this->data = $data;
+  }
+
+  /**
+   * Get the channels the event should broadcast on.
+   *
+   * @return Channel|array
+   */
+  public function broadcastOn() {
+    return ['new-notification'];
+  }
+
+  /**
+   * @return string
+   */
+  public function broadcastAS() {
+    /*
+     this is name allies for namespace of this event which you will deal with it
+     inside pusherNotification.js in line 10 in bind method
+    */
+    return 'my-event';
+  }
+}
