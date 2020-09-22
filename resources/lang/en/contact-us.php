ad1d1de852c522d284cdb1ae5c0376e4bd322db1
Mahmoud Mohamed Ramadan <mahmoudramadan496@gmail.com>
2020-09-22 23:59:17 +0200

    add new english langs files

---
+++ resources/lang/en/contact-us.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+  'Contact Us'     => 'Contact Us', 'labels.Name' => 'Name',
+  'labels.Email'   => 'Email', 'labels.Message' => 'Message',
+  'inputs.Name'    => 'Enter Your Name', 'inputs.Email' => 'Enter Your Email',
+  'inputs.Message' => 'Enter Your Message'
+];
