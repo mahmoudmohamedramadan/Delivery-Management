f775c8d7d4ed6aae5150f168777661cdd7b62e07
Mahmoud Mohamed Ramadan <mahmoudramadan496@gmail.com>
2020-09-23 00:11:54 +0200

    add payment view

---
+++ resources/views/project/payment/form.blade.php
@@ -0,0 +1,4 @@
+@section('main')
+<script src="https://test.oppwa.com/v1/paymentWidgets.js?checkoutId={{ $responseData['id'] }}"></script>
+<form action="/index/car-payment/{{ $id }}" class="paymentWidgets" data-brands="VISA MASTER AMEX"></form>
+@stop
