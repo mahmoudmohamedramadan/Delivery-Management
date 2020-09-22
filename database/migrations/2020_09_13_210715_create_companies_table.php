a846090af4b2dec151f3409e4073da744eb0737a
Mahmoud Mohamed Ramadan <mahmoudramadan496@gmail.com>
2020-09-22 23:40:03 +0200

    add companies migration file

---
+++ database/migrations/2020_09_13_210715_create_companies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('manager');
+            $table->string('address');
+            $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->string('product_name')->unique();
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
