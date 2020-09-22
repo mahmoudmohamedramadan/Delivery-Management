aee6466c58e7b5e19d0603d71e85a77cd4185873
Mahmoud Mohamed Ramadan <mahmoudramadan496@gmail.com>
2020-09-22 23:12:56 +0200

    add media-library routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -5,76 +5,102 @@ namespace App\Providers;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Route;
 
-class RouteServiceProvider extends ServiceProvider
-{
-    /**
-     * This namespace is applied to your controller routes.
-     *
-     * In addition, it is set as the URL generator's root namespace.
-     *
-     * @var string
-     */
-    protected $namespace = 'App\Http\Controllers';
+class RouteServiceProvider extends ServiceProvider {
+  /**
+   * This namespace is applied to your controller routes.
+   *
+   * In addition, it is set as the URL generator's root namespace.
+   *
+   * @var string
+   */
+  protected $namespace = 'App\Http\Controllers';
 
-    /**
-     * The path to the "home" route for your application.
-     *
-     * @var string
-     */
-    public const HOME = '/home';
+  /**
+   * The path to the "home" route for your application.
+   *
+   * @var string
+   */
+  public const HOME = '/home';
 
-    /**
-     * Define your route model bindings, pattern filters, etc.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
+  /**
+   * Define your route model bindings, pattern filters, etc.
+   *
+   * @return void
+   */
+  public function boot() {
+    //
 
-        parent::boot();
-    }
+    parent::boot();
+  }
 
-    /**
-     * Define the routes for the application.
-     *
-     * @return void
-     */
-    public function map()
-    {
-        $this->mapApiRoutes();
+  /**
+   * Define the routes for the application.
+   *
+   * @return void
+   */
+  public function map() {
+    $this->mapApiRoutes();
 
-        $this->mapWebRoutes();
+    $this->mapWebRoutes();
 
-        //
-    }
+    $this->mapAjaxRoutes();
 
-    /**
-     * Define the "web" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     *
-     * @return void
-     */
-    protected function mapWebRoutes()
-    {
-        Route::middleware('web')
-            ->namespace($this->namespace)
-            ->group(base_path('routes/web.php'));
-    }
+    $this->mapMechanicRoutes();
 
-    /**
-     * Define the "api" routes for the application.
-     *
-     * These routes are typically stateless.
-     *
-     * @return void
-     */
-    protected function mapApiRoutes()
-    {
-        Route::prefix('api')
-            ->middleware('api')
-            ->namespace($this->namespace)
-            ->group(base_path('routes/api.php'));
-    }
+    $this->mapSearchRoutes();
+
+    $this->mapCollectionRoutes();
+
+    $this->mapMediaLibraryRoutes();
+  }
+
+  /**
+   * Define the "web" routes for the application.
+   *
+   * These routes all receive session state, CSRF protection, etc.
+   *
+   * @return void
+   */
+  protected function mapWebRoutes() {
+    Route::middleware('web')->namespace($this->namespace)
+         ->group(base_path('routes/web.php'));
+  }
+
+  /**
+   * Define the "api" routes for the application.
+   *
+   * These routes are typically stateless.
+   *
+   * @return void
+   */
+  protected function mapApiRoutes() {
+    Route::prefix('api')->middleware('api')->namespace($this->namespace)
+         ->group(base_path('routes/api.php'));
+  }
+
+  protected function mapAjaxRoutes() {
+    //middleware('web) this means we will use the middleware of web inside Kernel.php if you want make another middleware  array like web add it inside middlewareGroups then write key of array here instead of web.
+    Route::middleware('web')->namespace($this->namespace)
+         ->group(base_path('routes/ajax.php'));
+  }
+
+  protected function mapMechanicRoutes() {
+    Route::middleware('web')->namespace($this->namespace)
+         ->group(base_path('routes/mechanic.php'));
+  }
+
+  protected function mapSearchRoutes() {
+    Route::middleware('web')->namespace($this->namespace)
+         ->group(base_path('routes/search.php'));
+  }
+
+  protected function mapCollectionRoutes() {
+    Route::middleware('web')->namespace($this->namespace)
+         ->group(base_path('routes/collection.php'));
+  }
+
+  protected function mapMediaLibraryRoutes() {
+    Route::middleware('web')->namespace($this->namespace)
+         ->group(base_path('routes/media-library.php'));
+  }
 }
