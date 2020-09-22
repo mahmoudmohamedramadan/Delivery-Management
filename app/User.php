e51d018dfaf6c3ad9d6e133089db95e195814789
Mahmoud Mohamed Ramadan <mahmoudramadan496@gmail.com>
2020-09-22 23:18:31 +0200

    add media library methods

--- app/User.php
+++ app/User.php
@@ -2,38 +2,56 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\MediaLibrary\HasMedia\HasMedia;
+use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
+use Spatie\MediaLibrary\MediaCollections\File;
 
-class User extends Authenticatable
+class User extends Authenticatable implements HasMedia
 {
-    use Notifiable;
+  use Notifiable, HasMediaTrait;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'name', 'email', 'phone', 'password', 'active'
+  ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @var array
+   */
+  protected $hidden = [
+    'password', 'remember_token',
+  ];
+
+  /**
+   * The attributes that should be cast to native types.
+   *
+   * @var array
+   */
+  protected $casts = [
+    'email_verified_at' => 'datetime',
+  ];
+
+  public function hasMedia(string $collectionName = 'default'): bool
+  {
+    return $this->getMedia($collectionName) ? false : true;
+  }
+
+  public function registerMediaCollections()
+  {
+    $this->addMediaCollection('avatar')
+      ->singleFile()
+      ->onlyKeepLatest(1)
+      ->acceptsFile(function (File $file) {
+        return $file->mimeType === 'image/jpg';
+      });
+  }
 }
