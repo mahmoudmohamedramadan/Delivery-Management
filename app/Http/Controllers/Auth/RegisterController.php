3e78ee9c10c66e98cd41c7e00952c9b82ca1daed
Mahmoud Mohamed Ramadan <mahmoudramadan496@gmail.com>
2020-09-22 22:44:11 +0200

    add auth controllers

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\User;
+
+class RegisterController extends Controller
+{
+  /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+  use RegistersUsers;
+
+  /**
+   * Where to redirect users after registration.
+   *
+   * @var string
+   */
+  protected $redirectTo = '/index';
+
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    $this->middleware('guest');
+  }
+
+  /**
+   * Get a validator for an incoming registration request.
+   *
+   * @param  array  $data
+   * @return \Illuminate\Contracts\Validation\Validator
+   */
+  protected function validator(array $data)
+  {
+    return Validator::make($data, [
+      'name' => ['required', 'string', 'max:255'],
+      'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+      'phone' => ['required', 'numeric', 'unique:users'],
+      'password' => ['required', 'string', 'min:8', 'confirmed'],
+    ],[
+      'name.required' => 'name field is required',
+      'email.required' => 'email field is required',
+      'phone.required' => 'phone field is required',
+      'password.required' => 'password field is required',
+    ]);
+  }
+
+  /**
+   * Create a new user instance after a valid registration.
+   *
+   * @param  array  $data
+   * @return \App\User
+   */
+  protected function create(array $data)
+  {
+    return User::create([
+      'name' => $data['name'],
+      'email' => $data['email'],
+      'phone' => $data['phone'],
+      'password' => Hash::make($data['password']),
+    ]);
+  }
+}
