a52853c35da25de636ce644e8d5b4c567afc54fc
Mahmoud Mohamed Ramadan <mahmoudramadan496@gmail.com>
2020-09-22 22:46:45 +0200

    add contact controller for send email

---
+++ app/Http/Controllers/Contact/ContactController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Contact;
+
+use App\Http\Controllers\Controller;
+use App\Notifications\UserNotification;
+use Illuminate\Support\Facades\Auth;
+use Mail;
+
+class ContactController extends Controller {
+  public function create() {
+    return view('project.contact');
+  }
+
+  public function contact() {
+    $data = request()->validate([
+      'name'    => 'required|max:25', 'email' => 'required|email',
+      'message' => 'required|max:255',
+    ]);
+    Mail::to('mahmoudramadan496@gmail.com')->send(new \App\Mail\ContactUs);
+    if (Mail::failures()) {
+      return redirect()->back()->with('error', 'mail can\'t send');
+    } else {
+      session()->flash('message',
+        'message sent successfully, we will keep in touch as soon as');
+      $this->notifyUser($data);
+      return back();
+    }
+  }
+
+  public function notifyUser($data) {
+    Auth::User()->notify(new UserNotification($data));
+  }
+}
