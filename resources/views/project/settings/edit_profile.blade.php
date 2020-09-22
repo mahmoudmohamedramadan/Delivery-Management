69806a13704578d1b686d98fd5edc65ced2e2450
Mahmoud Mohamed Ramadan <mahmoudramadan496@gmail.com>
2020-09-23 00:12:27 +0200

    add settings views

---
+++ resources/views/project/settings/edit_profile.blade.php
@@ -0,0 +1,52 @@
+@section('editProfileSection')
+    <form action="/index/ajax/settings/edit-profile/{{ auth()->user()->id }}">
+        @csrf
+        <div class="alert alert-success" style="display: none" id="flash-msg">
+            <strong>Success</strong><br>
+            <span>data saved sucessfully</span>
+        </div>
+        <label>
+            <span class="label-txt">Username</span>
+            <input type="text" value="{{ auth()->user()->name ?? old('name') }}" name="name" class="input"
+                autocomplete="off" placeholder="Enter UserName">
+            @error('name')
+            <span class="text-red-700 font-bold">{{ $message }}</span>
+            @enderror
+            <div class="line-box">
+                <div class="line"></div>
+            </div>
+        </label>
+        <label>
+            <span class="label-txt">Email</span>
+            <input type="text" value="{{ auth()->user()->email ?? old('email') }}" name="email" class="input"
+                autocomplete="off" placeholder="Enter Email">
+            @error('email')
+            <span class="text-red-700 font-bold">{{ $message }}</span>
+            @enderror
+            <div class="line-box">
+                <div class="line"></div>
+            </div>
+        </label>
+        <label>
+            <span class="label-txt">Phone Number</span>
+            <input type="text" value="{{ auth()->user()->phone ?? old('phone') }}" name="phone" class="input"
+                autocomplete="off" placeholder="Enter Phone Number">
+            @error('phone')
+            <span class="text-red-700 font-bold">{{ $message }}</span>
+            @enderror
+            <div class="line-box">
+                <div class="line"></div>
+            </div>
+        </label>
+        <label>
+            <div class="input-group">
+                <span class="label-txt">Avatar</span>
+                <input type="file" name="avatar" class="custom-file-input cursor-pointer" id="avatar-name">
+                <div class="line-box">
+                    <div class="line"></div>
+                </div>
+            </div>
+        </label>
+        <input type="submit" value="Edit Data" class="btn btn-success">
+    </form>
+@endsection
---
+++ resources/views/project/settings/index_settings.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.app')@section('title', 'Settings | Delivery Management')
+@section('ajax')
+    <script>
+        $(document).ready(function() {
+            $('#edit-profile').click(function(e) {
+                e.preventDefault();
+                $.ajax({
+                    url: '/index/ajax/settings/edit-profile',
+                    type: 'get',
+                    dataType: 'json',
+                    success: function(data) {
+                        if (data.status) {
+                            $('#content-settings').empty().html(data.editProfileSection);
+                        }
+                    },
+                    error: function(data) {
+                        console.log(data);
+                    },
+                });
+            });
+        });
+        $(document).on('submit', 'form', function(e) {
+          const image =
+            e.preventDefault();
+            $.ajax({
+                url: $(this).attr('action'),
+                type: 'post',
+                encType: 'multipart/form-data',
+                dataType: 'json',
+                data: new FormData($(this)[0]),
+                contentType: false,
+                cache: false,
+                processData: false,
+                success: function(data) {
+                    if (data.status) {
+                        var getFlashMsg = document.getElementById('flash-msg');
+                        $('#flash-msg').show();
+                        setTimeout(() => {
+                          $('#flash-msg').hide();
+                        }, 6000);
+                    }
+                },
+                error: function(data) {
+                    console.log(data);
+                },
+            });
+        });
+        /*
+        notice that you can't access any element in any view to other view which fetched using ajax using class or id like this #edit-profile but use upper method
+        */
+
+    </script>
+@stop
+@section('content')
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Delivery Management Settings</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" id="edit-profile" style="cursor: pointer">Edit Profile<span
+                            class="sr-only">(current)</span></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div id="content-settings"></div>
+@endsection
