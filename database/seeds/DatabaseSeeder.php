22068c54a0866f397c9a18d0aaae23b064ef78e5
Mahmoud Mohamed Ramadan <mahmoudramadan496@gmail.com>
2020-09-22 23:44:26 +0200

    update database seeder file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,15 +2,14 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UserSeeder::class);
-    }
+class DatabaseSeeder extends Seeder {
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run() {
+    $this->call(DelegateSeeder::class);
+    // $this->call(CompanySeeder::class);
+  }
 }
