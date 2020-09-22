ebf7e5d26ead11da206ea2511319c7c216437d73
Mahmoud Mohamed Ramadan <mahmoudramadan496@gmail.com>
2020-09-22 23:49:01 +0200

    update filesystems config file

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,13 @@ return [
             'visibility' => 'public',
         ],
 
+        'delegate' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/images/delegate/'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
