82755be94e267aadc9d960b2223e48581fdbbe21
Mahmoud Mohamed Ramadan <mahmoudramadan496@gmail.com>
2020-09-27 01:09:38 +0200

    customize DelegateController and render filter delegate section with new data

--- app/Http/Controllers/Delegate/DelegateController.php
+++ app/Http/Controllers/Delegate/DelegateController.php
@@ -2,49 +2,56 @@
 
 namespace App\Http\Controllers\Delegate;
 
-use App\Models\Delegate;
+use Alert;
 use App\Http\Controllers\Controller;
+use App\Models\Delegate;
 use App\Traits\Delegate\ValidateDelegateTrait;
 use Illuminate\Notifications\Notifiable;
 
 class DelegateController extends Controller
 {
-  use Notifiable, ValidateDelegateTrait;
+    use Notifiable, ValidateDelegateTrait;
 
-  public function index()
-  {
-    // data passed using View Composer(AppServiceProvider) in boot method(First Way).
-    return view('project.delegate.index_delegate')->with('filter', 0);
-  }
-
-  public function create()
-  {
-    return view('project.delegate.create_delegate')->with([
-      'cars' => \App\Models\Car::get(), 'car_count' => \App\Models\Car::count()
-    ]);
-  }
-
-  public function store()
-  {
-    /* notice that after execute whatever in listener it return again here from the place which you call event */
-    event(new \App\Events\DelegateEvent($this->validation()));
-    session()->flash('message', 'data saved successfully');
-    return redirect()->to('/index/delegate/create');
-  }
-
-  public function destroy($id)
-  {
-    $delegate = Delegate::findOrFail($id);
-    if ($delegate->image) {
-      unlink(public_path('images/delegate/' . $delegate->image));
+    public function index()
+    {
+        // data passed using View Composer(AppServiceProvider) in boot method(First Way).
+        $filterSection = view('project.delegate.filter_delegate')
+            ->with(['filter' => 0])
+            ->renderSections();
+        return view('project.delegate.index_delegate')
+            ->with('delegate_rows', $filterSection['filterSection']);
     }
-    $delegate->delete();
-    /*
-    whereNull whereNotNull whereIn >> keywords maybe you use
-    here when you call method of scopeId the return query will be replaced
-    with this id after scope resolution operator(::)
-     */
-    $delegate->orders()->delete();
-    return redirect()->back();
-  }
+
+    public function create()
+    {
+        return view('project.delegate.create_delegate')->with([
+            'cars' => \App\Models\Car::get(),
+        ]);
+    }
+
+    public function store()
+    {
+        /* notice that after execute whatever in listener it return again here from the place which you call event */
+        event(new \App\Events\DelegateEvent($this->validation()));
+        Alert::success('Success', 'data saved successfully');
+        return redirect()->to('/index/delegate/create');
+    }
+
+    // remember that dalagate updates using livewire
+
+    public function destroy($id)
+    {
+        $delegate = Delegate::findOrFail($id);
+        if ($delegate->image) {
+            unlink(public_path('images/delegate/' . $delegate->image));
+        }
+        $delegate->delete();
+        /*
+        whereNull whereNotNull whereIn >> keywords maybe you use
+        here when you call method of scopeId the return query will be replaced
+        with this id after scope resolution operator(::)
+         */
+        $delegate->orders()->delete();
+        return redirect()->back();
+    }
 }
