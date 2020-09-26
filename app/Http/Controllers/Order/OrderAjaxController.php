f98433a6116bf565f7316351edaf28bb0aa2edf0
Mahmoud Mohamed Ramadan <mahmoudramadan496@gmail.com>
2020-09-27 01:15:33 +0200

    add show method in OrderAjaxController to pass data to rendered view

--- app/Http/Controllers/Order/OrderAjaxController.php
+++ app/Http/Controllers/Order/OrderAjaxController.php
@@ -3,49 +3,61 @@
 namespace App\Http\Controllers\Order;
 
 use App\Http\Controllers\Controller;
+use App\Models\Delegate;
 use App\Models\Order;
 use App\Traits\Order\ValidateOrderTrait;
 
-class OrderAjaxController extends Controller {
-  use ValidateOrderTrait;
+class OrderAjaxController extends Controller
+{
+    use ValidateOrderTrait;
 
-  public function store() {
-    $order = Order::create($this->validation());
-    if ($order) {
-      return response()->json([
-        'status' => true
-      ]);
+    public function store()
+    {
+        Order::create($this->validation());
+        return response()->json([
+            'status' => true,
+        ]);
     }
-  }
 
-  public function update($id) {
-    $order = Order::findOrFail($id);
-    $order->update($this->validation());
-    return response()->json([
-      'status' => true
-    ]);
-  }
-
-  public function destroy($id) {
-    $order = Order::findOrFail($id);
-    $order->delete();
-    return response()->json([
-      'status' => true
-    ]);
-  }
-
-  public function search() {
-    $orders = Order::where('shop_name', 'like',
-      '%' . request()->get('searchVal') . '%')->get();
-    try {
-      $searchSection = view('project.order.search_order', [
-        'orders' => $orders
-      ])->renderSections();
-    } catch (\Throwable $e) {
-      dd($e);
+    public function show(Delegate $delegate)
+    {
+        $orderSection = view('project.order.filter_order')->with(['orders' => $delegate->orders])
+            ->renderSections();
+        return response()->json([
+            'status' => true,
+            'orders' => $orderSection['orderSection'],
+        ]);
     }
-    return response([
-      'searchSection' => $searchSection['searchSection']
-    ]);
-  }
+
+    public function update($id)
+    {
+        Order::findOrFail($id)->update($this->validation());
+        return response()->json([
+            'status' => true,
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        Order::findOrFail($id)->delete();
+        return response()->json([
+            'status' => true,
+        ]);
+    }
+
+    public function search()
+    {
+        $orders = Order::where('shop_name', 'like',
+            '%' . request()->get('searchVal') . '%')->get();
+        try {
+            $searchSection = view('project.order.search_order', [
+                'orders' => $orders,
+            ])->renderSections();
+        } catch (\Throwable $e) {
+            dd($e);
+        }
+        return response([
+            'searchSection' => $searchSection['searchSection'],
+        ]);
+    }
 }
