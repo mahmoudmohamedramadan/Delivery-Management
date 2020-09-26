16463740e2a571fcdcebecdab550b9c5a8c8286b
Mahmoud Mohamed Ramadan <mahmoudramadan496@gmail.com>
2020-09-27 01:03:32 +0200

    reformate CompanyAjaxController file

--- app/Http/Controllers/Company/CompanyAjaxController.php
+++ app/Http/Controllers/Company/CompanyAjaxController.php
@@ -10,57 +10,58 @@ use Spatie\QueryBuilder\QueryBuilder;
 class CompanyAjaxController extends Controller
 {
 
-  public function index()
-  {
-    return view('project.company.index_company')->with('companies', Company::paginate(15));
-  }
+    public function index()
+    {
+        return view('project.company.index_company')->with('companies', Company::get());
+    }
 
-  public function search()
-  {
-    /*
-       here if id equals 1 and there are 20 companies this query will return company which contains 1
-       but if you want filter with given exact id do this...
-       =================================
-       $compnay = Query::for(Company::class)
+    public function search()
+    {
+        /*
+        here if id equals 1 and there are 20 companies this query will return company which contains 1
+        but if you want filter with given exact id do this...
+        =================================
+        $compnay = Query::for(Company::class)
         ->allowedFilters(AllowedFilter::exact('id'))
         ->get();
-       */
-    $companies = QueryBuilder::for(Company::class)
-      ->allowedFilters(['id', 'name', 'email', 'phone'])
-      ->get();
-    $getSection = view('project.company.search-company')
-      ->with('companies', $companies)
-      ->renderSections();
-    return response()
-      ->json(['companies' => $getSection]);
-  }
+         */
+        $companies = QueryBuilder::for(Company::class)
+                ->allowedFilters(['id', 'name', 'email', 'phone'])
+                ->get();
 
-  public function create()
-  {
-    return view('project.company.create_company');
-  }
+        $getSection = view('project.company.search-company')
+            ->with('companies', $companies)
+            ->renderSections();
+        return response()
+            ->json(['companies' => $getSection]);
+    }
 
-  public function store()
-  {
-  }
+    public function create()
+    {
+        return view('project.company.create_company');
+    }
 
-  public function edit($id)
-  {
-    $company = Company::findOrFail($id);
-    return view('project.company.edit_company', compact('company'));
-  }
+    public function store()
+    {
+    }
 
-  public function update(CompanyRequest $request, $id)
-  {
-    $company = Company::findOrFail($id);
-    $company->fill($request->validated())->save();
-    return response()->json(['status' => true]);
-  }
+    public function edit($id)
+    {
+        $company = Company::findOrFail($id);
+        return view('project.company.edit_company', compact('company'));
+    }
 
-  public function destroy()
-  {
-    Company::findOrFail(request()->get('searchCompany'))->delete();
-    return response()
-      ->json(['status' => true]);
-  }
+    public function update(CompanyRequest $request, $id)
+    {
+        $company = Company::findOrFail($id);
+        $company->fill($request->validated())->save();
+        return response()->json(['status' => true]);
+    }
+
+    public function destroy()
+    {
+        Company::findOrFail(request()->get('searchCompany'))->delete();
+        return response()
+            ->json(['status' => true]);
+    }
 }
