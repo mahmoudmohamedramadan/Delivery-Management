1d36c0190cb0656be1bb1e0bac0518e16fe6b4c6
Mahmoud Mohamed Ramadan <mahmoudramadan496@gmail.com>
2020-09-27 01:23:15 +0200

    reformate DelegatePolicy file

--- app/Policies/DelegatePolicy.php
+++ app/Policies/DelegatePolicy.php
@@ -6,81 +6,89 @@ use App\Delegate;
 use App\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
-class DelegatePolicy {
-  use HandlesAuthorization;
+class DelegatePolicy
+{
+    use HandlesAuthorization;
 
-  /**
-   * Determine whether the user can view any models.
-   *
-   * @param \App\User $user
-   * @return mixed
-   */
-  public function viewAny(User $user) {
-    //
-  }
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param \App\User $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
 
-  /**
-   * Determine whether the user can view the model.
-   *
-   * @param \App\User $user
-   * @param \App\Delegate $delegate
-   * @return mixed
-   */
-  public function view(User $user, Delegate $delegate) {
-    //
-  }
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param \App\User $user
+     * @param \App\Delegate $delegate
+     * @return mixed
+     */
+    public function view(User $user, Delegate $delegate)
+    {
+        //
+    }
 
-  /**
-   * Determine whether the user can create models.
-   *
-   * @param \App\User $user
-   * @return mixed
-   */
-  public function create(User $user) {
-    return $user->email === 'mahmoudramadan496@gmail.com';
-  }
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param \App\User $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        return $user->email === 'mahmoudramadan496@gmail.com';
+    }
 
-  /**
-   * Determine whether the user can update the model.
-   *
-   * @param \App\User $user
-   * @param \App\Delegate $delegate
-   * @return mixed
-   */
-  public function update(User $user, Delegate $delegate) {
-    //
-  }
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param \App\User $user
+     * @param \App\Delegate $delegate
+     * @return mixed
+     */
+    public function update(User $user, Delegate $delegate)
+    {
+        //
+    }
 
-  /**
-   * Determine whether the user can delete the model.
-   *
-   * @param \App\User $user
-   * @param \App\Delegate $delegate
-   * @return mixed
-   */
-  public function delete(User $user, Delegate $delegate) {
-    //
-  }
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param \App\User $user
+     * @param \App\Delegate $delegate
+     * @return mixed
+     */
+    public function delete(User $user, Delegate $delegate)
+    {
+        //
+    }
 
-  /**
-   * Determine whether the user can restore the model.
-   *
-   * @param \App\User $user
-   * @param \App\Delegate $delegate
-   * @return mixed
-   */
-  public function restore(User $user, Delegate $delegate) {
-    //
-  }
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param \App\User $user
+     * @param \App\Delegate $delegate
+     * @return mixed
+     */
+    public function restore(User $user, Delegate $delegate)
+    {
+        //
+    }
 
-  /**
-   * Determine whether the user can permanently delete the model.
-   *
-   * @param \App\User $user
-   * @param \App\Delegate $delegate
-   * @return mixed
-   */
-  public function forceDelete(User $user, Delegate $delegate) {
-    //
-  }
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param \App\User $user
+     * @param \App\Delegate $delegate
+     * @return mixed
+     */
+    public function forceDelete(User $user, Delegate $delegate)
+    {
+        //
+    }
 }
