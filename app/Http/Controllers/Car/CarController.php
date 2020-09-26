f139f0e5ecafb3ca6f6249f2013045ac8113aeb2
Mahmoud Mohamed Ramadan <mahmoudramadan496@gmail.com>
2020-09-27 01:02:17 +0200

    customize CarController file

--- app/Http/Controllers/Car/CarController.php
+++ app/Http/Controllers/Car/CarController.php
@@ -3,74 +3,72 @@
 namespace App\Http\Controllers\Car;
 
 use App\Http\Controllers\Controller;
+use App\Models\Car;
 
 class CarController extends Controller
 {
     public function index()
     {
-        $cars = \App\Models\Car::get();
+        $cars = Car::get();
         return view('project.car.index_car')->with('cars', $cars);
     }
     public function create()
     {
-        $last_car = \App\Models\Car::max('id') + 1;
+        $last_car = Car::max('id') + 1;
         $car_transaction = \App\Models\CarTransactoin::find($last_car);
         $mechanics = \App\Models\Mechanic::get();
         if ($mechanics && $car_transaction) {
             return view('project.car.create_car')->with([
                 'error' => false,
                 'payment_error' => false,
-                'mechanics' => $mechanics
+                'mechanics' => $mechanics,
             ]);
         } else if (!$car_transaction) {
             return view('project.car.create_car')->with([
-                 'error' => false,
+                'error' => false,
                 'payment_error' => true,
-                'mechanics' => $mechanics
+                'mechanics' => $mechanics,
             ]);
         } else {
             return view('project.car.create_car')->with([
                 'error' => true,
                 'payment_error' => true,
-                'mechanics' => $mechanics
+                'mechanics' => $mechanics,
             ]);
         }
     }
+
     public function store()
     {
-        \App\Models\Car::create($this->validation());
+        Car::create($this->validation());
         return redirect()->back()->with('message', 'data saved successfully');
     }
+
     public function edit($id)
     {
-        $car = \App\Models\Car::findOrFail($id);
-        if ($car) {
-            return view('project.car.edit_car', compact('car'));
-        }
+        $car = Car::findOrFail($id);
+        $mechanics = \App\Models\Mechanic::get()->except($car->mechanic_id);
+        return view('project.car.edit_car', compact('car', 'mechanics'));
     }
+
     public function update($id)
     {
-        $car = \App\Models\Car::findOrFail($id);
-        if ($car) {
-            $car->update($this->validation());
-            return redirect()->to('/index/car');
-        }
+        Car::findOrFail($id)->update($this->validation());
+        return redirect()->to('/index/car');
     }
+
     public function destroy($id)
     {
-        $car = \App\Models\Car::findOrFail($id);
-        if ($car) {
-            $car->delete();
-        }
+        Car::findOrFail($id)->delete();
     }
+
     public function validation()
     {
-        $data = request()->validate([
-            'type' => 'required|unique:cars,type',
+        return request()->validate([
+            'type' => 'required',
             'char' => 'required|numeric',
-            'number' => 'required|numeric|unique:cars,number',
-            'mechanic_id' => 'required'
+            'number' => 'required|numeric',
+            'mechanic_id' => 'required',
         ]);
-        return $data;
     }
 }
