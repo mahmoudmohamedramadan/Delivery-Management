f5c420be5432ed483fb3c6336ddc0487ce24bb5e
Mahmoud Mohamed Ramadan <mahmoudramadan496@gmail.com>
2020-09-27 01:18:20 +0200

    reformate UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,32 +6,32 @@ use App\User;
 
 class UserController extends Controller
 {
-  public function editProfileForm()
-  {
-    $editProfileSection = view('project.settings.edit_profile')->renderSections();
-    return response()->json([
-      'status' => true,
-      'editProfileSection' => $editProfileSection['editProfileSection']
-    ]);
-  }
+    public function editProfileForm()
+    {
+        $editProfileSection = view('project.settings.edit_profile')->renderSections();
+        return response()->json([
+            'status' => true,
+            'editProfileSection' => $editProfileSection['editProfileSection'],
+        ]);
+    }
 
-  public function update(User $user)
-  {
-    $user->update([
-      'name' => request()->name,
-      'email' => request()->email,
-      'phone' => request()->phone
-    ]);
-    $user->addMedia(request()->avatar)
-      ->toMediaCollection('avatar');
-    return response()->json([
-      'status' => true
-    ]);
-  }
+    public function update(User $user)
+    {
+        $user->update([
+            'name' => request()->name,
+            'email' => request()->email,
+            'phone' => request()->phone,
+        ]);
+        $user->addMedia(request()->avatar)
+            ->toMediaCollection('avatar');
+        return response()->json([
+            'status' => true,
+        ]);
+    }
 
-  public function notify()
-  {
-    auth()->user()
-      ->notify(new \App\Notifications\UserNotification(auth()->user()->name));
-  }
+    public function notify()
+    {
+        auth()->user()
+            ->notify(new \App\Notifications\UserNotification(auth()->user()->name));
+    }
 }
