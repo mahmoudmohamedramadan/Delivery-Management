e7cef126d13dc4edc9aea4b98af12ec22a998199
Mahmoud Mohamed Ramadan <mahmoudramadan496@gmail.com>
2020-09-27 01:21:12 +0200

    reformate DelegateListener file

--- app/Http/Controllers/Mechanic/MechanicAjaxController.php
+++ app/Http/Controllers/Mechanic/MechanicAjaxController.php
@@ -4,39 +4,38 @@ namespace App\Http\Controllers\Mechanic;
 
 use App\Http\Controllers\Controller;
 use App\Models\Mechanic;
-use Spatie\QueryBuilder\QueryBuilder;
 
 class MechanicAjaxController extends Controller
 {
-  public function store()
-  {
-    Mechanic::create($this->validation());
-    return response()->json([
-      'status' => true
-    ]);
-  }
+    public function store()
+    {
+        Mechanic::create($this->validation());
+        return response()->json([
+            'status' => true,
+        ]);
+    }
 
-  public function update($id)
-  {
-    Mechanic::findOrFail($id)->update(request()->all());
-    return response()->json([
-      'status' => true
-    ]);
-  }
+    public function update($id)
+    {
+        Mechanic::findOrFail($id)->update(request()->all());
+        return response()->json([
+            'status' => true,
+        ]);
+    }
 
-  public function destroy($id)
-  {
-    $mechanic = Mechanic::findOrFail($id);
-    $mechanic->delete();
-    return response()->json([
-      'status' => true
-    ]);
-  }
+    public function destroy($id)
+    {
+        $mechanic = Mechanic::findOrFail($id);
+        $mechanic->delete();
+        return response()->json([
+            'status' => true,
+        ]);
+    }
 
-  public function validation()
-  {
-    return request()->validate([
-      'name' => 'required', 'address' => 'required'
-    ]);
-  }
+    public function validation()
+    {
+        return request()->validate([
+            'name' => 'required', 'address' => 'required',
+        ]);
+    }
 }
--- app/Listeners/DelegateListener.php
+++ app/Listeners/DelegateListener.php
@@ -2,35 +2,34 @@
 
 namespace App\Listeners;
 
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
+class DelegateListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
 
-class DelegateListener {
-  /**
-   * Create the event listener.
-   *
-   * @return void
-   */
-  public function __construct() {
-    //
-  }
-
-  /**
-   * Handle the event.
-   *
-   * @param object $event
-   * @return void
-   */
-  public function handle($event)
-  : void {
-    $delegate = \App\Models\Delegate::create($event->data);
-    // image uploaded using mutators.
-    $car = \App\Models\Car::find(request('car_id'));
-    $delegate->cars()->attach($car);
-    #region SimilarToAttach
-    // $delegate->cars()->detach($car);
-    // $delegate->cars()->sync([1]);
-    // $delegate->cars()->syncWithoutDetaching([1,2,3]);
-    #endregion
-  }
+    /**
+     * Handle the event.
+     *
+     * @param object $event
+     * @return void
+     */
+    public function handle($event)
+    {
+        $delegate = \App\Models\Delegate::create($event->data);
+        // image uploaded using mutators.
+        $car = \App\Models\Car::find(request('car_id'));
+        $delegate->cars()->attach($car);
+        #region SimilarToAttach
+        // $delegate->cars()->detach($car);
+        // $delegate->cars()->sync([1]);
+        // $delegate->cars()->syncWithoutDetaching([1,2,3]);
+        #endregion
+    }
 }
