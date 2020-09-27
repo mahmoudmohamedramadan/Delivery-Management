5d0fb04443c69fbf2f71eb76353b2e3f19da885e
Mahmoud Mohamed Ramadan <mahmoudramadan496@gmail.com>
2020-09-27 23:58:27 +0200

    add modal and ajax request with orders in layout view

--- resources/views/project/layouts/layout.blade.php
+++ resources/views/project/layouts/layout.blade.php
@@ -1,13 +1,56 @@
+<script>
+    $(document).ready(function() {
+        $('#delegate-order-{{ $delegate->id }}').click(function() {
+            $.ajax({
+                url: '/index/ajax/delegate-order/{{ $delegate->id }}',
+                type: 'get',
+                dataType: 'json',
+                data: {},
+                success: function(data) {
+                    if (data.status) {
+                        console.log(data.orders);
+                        $('.modal-body').empty().html(data.orders);
+                        $('#delegate-order').modal('show');
+                    }
+                },
+                error: function(data) {
+                    console.log(data);
+                },
+            });
+        });
+    });
+
+</script>
+
 <td>
-    <form action="/index/delegate/{{ $id }}" method="POST" style="width:auto;margin:0px;padding:0px;background:none">
+    <form action="/index/delegate/{{ $delegate->id }}" method="POST"
+        style="width:auto;margin:0px;padding:0px; background:none">
         @csrf
         @method('DELETE')
-        @if ($filter === 2)
-            <input type="button" value="Show Orders" class="btn btn-success">
+        @if ($delegate->orders->count() > 0)
+            <button type="button" class="btn btn-success" data-toggle="modal" id="delegate-order-{{ $delegate->id }}">
+                Orders
+            </button>
         @endif
-        <input type="button" value="Edit Data" class="btn btn-success"
-            onclick="location.href = '/index/delegate/{{ $delegate->id }}/edit' ">
         <input type="submit" value="Delete" class="btn btn-danger">
-        <input type="button" value="Delete Ajax" class="delete-ajax btn btn-danger" delete-id="{{ $id }}">
+        <input type="button" value="Delete Ajax" class="delete-ajax btn btn-danger" delete-id="{{ $delegate->id }}">
     </form>
 </td>
+<!-- Modal -->
+<div class="modal fade" id="delegate-order" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-xl" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">{{ $delegate->name }} Orders</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body"></div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
