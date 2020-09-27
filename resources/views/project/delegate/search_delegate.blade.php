b4f40639601bf146822041f3c2280c777135088f
Mahmoud Mohamed Ramadan <mahmoudramadan496@gmail.com>
2020-09-27 23:54:34 +0200

    reformate search_delegate view

--- resources/views/project/delegate/search_delegate.blade.php
+++ resources/views/project/delegate/search_delegate.blade.php
@@ -1,10 +1,9 @@
 @section('searchSection')
-  <ul class="dropdown-menu" style="width:260px;display:block;
-  position:relative">
+  <ul class="dropdown-menu" style="width:260px;display:block;position:relative">
     @foreach($delegates as $delegate)
-      <li style="cursor:pointer"><span style="margin:25px">{{
-      $delegate->name
-      }}</span></li>
+      <li class="li-delegate-name" style="cursor:pointer">
+        <span style="margin:25px">{{ $delegate->name }}</span>
+      </li>
     @endforeach
   </ul>
 @endsection
