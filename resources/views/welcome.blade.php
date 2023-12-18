eef23d5dac0a7c14f13d9952ddca6326b7f2b5ff
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2023-12-19 00:36:06 +0200

    Use the new icon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Icon -->
-    <link rel="icon" href="{{ asset('delivery-management.jfif') }}">
+    <link rel="icon" href="{{ asset('delivery-management.png') }}">
 
     <title>Welcome Page | Delivery Management</title>
 
